8748f5832aba67bc54a1aad1dcad7f3724fc8769
pepopln <petarmiroslavovnqgolov@abv.bg>
2016-09-01 15:12:50 +0300

    s

--- OOP/Interfaces/src/BorderControl/Main.java
+++ OOP/Interfaces/src/BorderControl/Main.java
@@ -12,32 +12,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Checker> citizens = new ArrayList<>();
+        List<Birthable> borns = new ArrayList<>();
         String[] info = reader.readLine().split("\\s+");
         while (true) {
 
             if (info[0].equals("End")) {
                 break;
             }
-            if (info.length == 2) {
-                citizens.add(new Robot(info[0], info[1]));
-            } else {
-                citizens.add(new Person(info[0], info[1], info[2]));
-            }
-
+          switch (info[0]){
+              case "Citizen":
+                  borns.add(new Person(info[1],info[2],info[3],info[4]));
+                  break;
+              case "Pet":
+                  borns.add(new Pet(info[1],info[2]));
+                  break;
+          }
 info=reader.readLine().split("\\s+");
         }
         String fakeNumber = reader.readLine();
         StringBuilder sb = new StringBuilder();
-        for (Checker citizen : citizens) {
+        for (Birthable birthable : borns) {
 
-            if (citizen.getId().equals(fakeNumber)||citizen.getId().substring(citizen.getId().length() - 3).equals(fakeNumber)) {
+            if (birthable.getBirthdate().equals(fakeNumber)||birthable.getBirthdate().endsWith(fakeNumber)) {
 
-                sb.append(citizen.getId());
+                sb.append(birthable.getBirthdate());
                 sb.append(System.lineSeparator());
 
             }
         }
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 }
--- OOP/Interfaces/src/BorderControl/Person.java
+++ OOP/Interfaces/src/BorderControl/Person.java
@@ -1,18 +1,31 @@
 package BorderControl;
 
 
-public class Person implements Checker {
-String name;
-    String age;
-    String id;
+public class Person implements Checker,Birthable {
+private String name;
+    private String age;
+    private String id;
+    private String birthDate;
 
-    public Person(String name, String age, String id) {
+    public Person(String name, String age, String id, String birthDate) {
         this.name = name;
         this.age = age;
         this.id = id;
+        this.birthDate = birthDate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getBirthdate() {
+        return this.birthDate;
+    }
+
     @Override
     public String getId() {
         return id;
     }
 }
+
